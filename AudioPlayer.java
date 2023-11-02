233546e2101ba52063ad22856e5ee762f9fc10d3
treezyyy <122749977+treezyyy@users.noreply.github.com>
2023-11-02 10:54:35 +0300

    Add files via upload

---
+++ AudioPlayer.java
@@ -0,0 +1,22 @@
+
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class AudioPlayer {
+    public static void main(String[] args) {
+        try (FileInputStream inputStream = new FileInputStream("E:\\musor\\music\\src\\musiсc0.mp3")) {
+            try {
+                Player player = new Player(inputStream);
+                player.play();
+            } catch (JavaLayerException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
